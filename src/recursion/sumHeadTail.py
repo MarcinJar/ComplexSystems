089006d2e6b56d1954f36103b27a11587552721d
Marcin Jarmułowski <m.jarmulowski@gmail.com>
2024-09-14 19:40:30 +0200

    Add sumHeadTail with recursive sum function

---
+++ src/recursion/sumHeadTail.py
@@ -0,0 +1,22 @@
+def sum(numbers: list[int]) -> int:
+    """
+    Calculates the sum of all numbers in the given list.
+    Args:
+        numbers (list[int]): A list of integers.
+    Returns:
+        int: The sum of all numbers in the list.
+    """
+    if len(numbers) == 0:
+        return 0
+    
+    head, tail = numbers[0], numbers[1:]
+    return head + sum(tail)
+
+nums = [1, 2, 3, 4, 5]
+print('Sum', nums, 'is', sum(nums))
+
+nums = [5, 2, 4, 8]
+print('Sum', nums, 'is', sum(nums))
+
+nums = [1, 10, 100, 1000]
+print('Sum', nums, 'is', sum(nums))
